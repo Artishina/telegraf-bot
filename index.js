d359063d3833865b66a991dbd2e0e3b8a60dc1b5
Polina Borisova <artive112@yandex.ru>
2020-07-01 15:51:39 +0300

    Initial project version

---
+++ index.js
@@ -0,0 +1,52 @@
+const Telegraf = require("telegraf");
+const bot = new Telegraf("1296204708:AAFWJFk84wx79nv0EYucT2VxA8yorefbB_Y");
+
+bot.use(async (ctx, next)=> {
+    ctx.reply('Middleware');
+    ctx.state.isSend = true;
+    ctx.state.role = ctx.message.message_id;
+    console.log(new Date());
+    await next();
+})
+
+bot.start(ctx=> {
+    const { state } = ctx;
+    console.log(state);
+    ctx.reply(`Start command ${state.isSend}`)
+});
+
+bot.help(ctx=>ctx.reply('Help command'));
+bot.settings(ctx=>ctx.reply('Settings'));
+
+//bot.command('stop',ctx=>ctx.reply('Stop command'));
+
+bot.command(['stop', 'finish'], (ctx)=>{
+    return ctx.reply('Stop command');
+})
+
+bot.mention('ravioli117', (ctx)=> {
+    ctx.reply("This is little piggy");
+})
+
+bot.phone('+79375217792', (ctx)=> {
+    ctx.reply("my phone number");
+})
+
+bot.hashtag('piggy', (ctx)=> {
+    ctx.reply('piggy hashtag');
+})
+
+bot.command('hello', (ctx)=> {
+    console.log(ctx);
+    ctx.reply(`Hello,${ctx.update.message.from.first_name}`);
+})
+
+bot.hears('piggy', (ctx)=> {
+    ctx.reply('PIGGY!!!');
+});
+
+bot.launch()
+    .then((res) => {
+        console.log("Started");
+    })
+    .catch((err) => console.log(err));
